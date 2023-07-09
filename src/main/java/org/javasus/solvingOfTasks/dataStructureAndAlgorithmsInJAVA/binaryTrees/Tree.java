2d3caffab055cab6c1ee50b4cf592ec48d1290bc
Roman NSK <roman.wap88@gmail.com>
2023-07-09 23:30:08 +0300

    BinaryTrees / Added methods on Tree.java and TreeApp.java
    Added insert, traverse, displayTree, minimum and maximum in Tree.java.
    Added main, getString, getChar and get Int method.

--- src/main/java/org/javasus/solvingOfTasks/dataStructureAndAlgorithmsInJAVA/binaryTrees/Tree.java
+++ src/main/java/org/javasus/solvingOfTasks/dataStructureAndAlgorithmsInJAVA/binaryTrees/Tree.java
@@ -1,5 +1,7 @@
 package org.javasus.solvingOfTasks.dataStructureAndAlgorithmsInJAVA.binaryTrees;
 
+import java.util.Stack;
+
 public class Tree {
   private Node root;
 
@@ -26,7 +28,33 @@ public class Tree {
 
   // insert node
   public void insert(int id, double dd) {
-
+    Node newNode = new Node();
+    newNode.iData = id;
+    newNode.dData = dd;
+    if (root == null) {
+      root = newNode;
+    } else {
+      Node current = root;
+      Node parent;
+      while (true) {
+        parent = current;
+        if (id < current.iData) {
+          current = current.leftChild;
+          if (current == null) {
+            parent.leftChild = newNode;
+            return;
+          }
+        } else {
+          current = current.rightChild;
+          if (current == null) {
+            parent.rightChild = newNode;
+            return;
+          }
+        }
+      }
+    }
+//    Time complexity O(Log2N)
+//    Space complexity O(1)
   }
 
   // Delete node
@@ -39,26 +67,108 @@ public class Tree {
     return new Node();
   }
 
+  public void minimum () {
+    Node current, last = null;
+    current = root;
+    while (current != null) {
+      last = current;
+      current = current.leftChild;
+    }
+    System.out.println( last.iData);
+  }
+
+  public void maximum () {
+    Node current, last = null;
+    current = root;
+    while (current != null) {
+      last = current;
+      current = current.rightChild;
+    }
+
+    System.out.println(last.iData);
+  }
+
   // Варианты вывода узлов дерева в зависимости от выбранного типа:
   // PreOrder (Прямой), InOrder (симметричный, по умолчанию), PostOrder (Обратный).
-  public void traverse (int traverseType) {
-
+  public void traverse (String traverseType) {
+    switch(traverseType) {
+    case "pre order"  : System.out.print("Pre order: ");
+                        preOrder(root);
+                        break;
+    case "in order"   : System.out.print("In order: ");
+                        inOrder(root);
+                        break;
+    case "post order" : System.out.print("Post order: ");
+                        postOrder(root);
+                        break;
+    }
+    System.out.println("\n _________________________________________________");
   }
 
   private void preOrder (Node localRoot) {
-
+    if (localRoot != null) {
+      System.out.print(localRoot.iData + " ");
+      preOrder(localRoot.leftChild);
+      preOrder(localRoot.rightChild);
+    }
   }
 
   private void inOrder (Node localRoot) {
-
+    if (localRoot != null) {
+      inOrder(localRoot.leftChild);
+      System.out.print(localRoot.iData + " ");
+      inOrder(localRoot.rightChild);
+    }
   }
 
   private void postOrder (Node localRoot) {
-
+    if (localRoot != null) {
+      postOrder(localRoot.leftChild);
+      postOrder(localRoot.rightChild);
+      System.out.print(localRoot.iData + " ");
+    }
   }
 
   // Вывод дерева на экран
   public void displayTree() {
+    Stack globalStack = new Stack();
+    globalStack.push(root);
+    int nBlanks = 32;
+    boolean isRowEmpty = false;
+    System.out.println("................................................................");
+    while (isRowEmpty == false) {
+      Stack localStack = new Stack();
+      isRowEmpty = true;
+      for (int j = 0; j  < nBlanks; j ++) {
+        System.out.print(' ');
+      }
+      while (globalStack.isEmpty() == false) {
+        Node temp = (Node) globalStack.pop();
+        if (temp != null) {
+          System.out.print(temp.iData);
+          localStack.push(temp.leftChild);
+          localStack.push(temp.rightChild);
 
+          if (temp.leftChild != null || temp.rightChild != null) {
+            isRowEmpty = false;
+          }
+        } else {
+          System.out.print("__");
+          localStack.push(null);
+          localStack.push(null);
+        }
+        for (int j = 0; j < nBlanks * 2 - 2; j++) {
+          System.out.print(' ');
+        }
+      }
+      System.out.println();
+      nBlanks /= 2;
+      while (localStack.isEmpty() == false) {
+        globalStack.push(localStack.pop());
+      }
+
+    }
+    System.out.println("................................................................");
   }
+
 }
--- src/main/java/org/javasus/solvingOfTasks/dataStructureAndAlgorithmsInJAVA/binaryTrees/TreeApp.java
+++ src/main/java/org/javasus/solvingOfTasks/dataStructureAndAlgorithmsInJAVA/binaryTrees/TreeApp.java
@@ -1,7 +1,79 @@
 package org.javasus.solvingOfTasks.dataStructureAndAlgorithmsInJAVA.binaryTrees;
 
-public class TreeApp {
-  public static void main (String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class TreeApp {
+  public static void main (String[] args) throws IOException {
+    int value;
+    String trChoice;
+    Tree theTree = new Tree();
+//    theTree.insert(50, 1.2);
+//    theTree.insert(25, 1.3);
+//    theTree.insert(15, 1.4);
+//    theTree.insert(35, 1.5);
+//    theTree.insert(5, 1.6);
+//    theTree.insert(20, 1.7);
+//    theTree.insert(30, 1.8);
+//    theTree.insert(40, 1.9);
+    theTree.insert(50, 1.1);
+    theTree.insert(30, 1.1);
+    theTree.insert(20, 1.1);
+    theTree.insert(40, 1.1);
+    theTree.insert(60, 1.1);
+    while (true) {
+      System.out.print("Введите букву выражения для показа, ");
+      System.out.print("i - insert, f - find, d -delete, t - traverse, s - show tree, max - show max root, min - show min root. \n");
+      String choice = getString();
+      switch (choice) {
+      case "s" :
+        theTree.displayTree();
+        break;
+      case "i" :
+        System.out.print("Введите значение для вставки:");
+        value = getInt();
+        theTree.insert(value, value + 0.9);
+        break;
+      case "f" :
+        System.out.print("Введите  значение для поиска: ");
+        value = getInt();
+        Node found = theTree.find(value);
+        if (found != null) {
+          System.out.print("Найден: " );
+          found.displayNode();
+          System.out.print("\n");
+        }
+      case "t" :
+        System.out.println("Выбери порядок вывода элементов: \n1. pre order - прямой. \n2. in order - симметричный. \n3. post order - обратный");
+        trChoice = getString();
+        theTree.traverse(trChoice);
+        break;
+      case "min" :
+        theTree.minimum();
+        break;
+      case "max" :
+        theTree.maximum();
+        break;
+      }
+    }
   }
+
+  public static String getString() throws IOException {
+    InputStreamReader reader = new InputStreamReader(System.in);
+    BufferedReader br = new BufferedReader(reader);
+    String st = br.readLine();
+    return st;
+  }
+
+  public static char getChar() throws IOException {
+    String s = getString();
+    return s.charAt(0);
+  }
+
+  public static int getInt() throws IOException {
+    String str = getString();
+    return Integer.parseInt(str);
+  }
+
 }
